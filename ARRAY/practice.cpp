862c1660a7434f78369b08cbacfe04d38243cda1
keyur sanghani <keyur.sanghani10@gmail.com>
2023-01-08 15:18:51 +0530

    8-1-2023

---
+++ ARRAY/practice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+                    
+                     
+                     
+                     
+int main()
+{
+   array<int,5> arr={1,2,3,5,4};
+    for(auto it:arr.begin();it!=arr.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+}
